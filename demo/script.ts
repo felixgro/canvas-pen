db681294f2bf66f47b2884429282c0f07d541fff
felixgro <me@felixgrohs.dev>
2021-11-13 13:54:53 +0100

    add publish workflow

--- demo/script.ts
+++ demo/script.ts
@@ -10,36 +10,11 @@ const pen = new Pen(canvas);
 // these may be changed any time during execution
 pen.setFillStyle('#000')
    .setStrokeStyle('#ccc')
-   .setLineWidth(5)
-   .setLineJoin('round');
+   .setStrokeWidth(5)
+   .setStrokeJoin('round');
 
 // draw a filled circle using an origin point and radius
 // all points passed to the pen are relative to the canvas
 // and are defined as [x, y]
-pen.circle([200, 200], 100)
-   .fill();
-
-// draw the outline of a square using
-// an origin point and side length
-pen.square([200, 200], 250)
-   .stroke();
-
-// draw any shape using 2 or more points
-// example: outline of triangle
-pen.polygon(
-   [250, 270],
-   [200, 120],
-   [150, 270]
-).stroke();
-
-// temporary change styles for a single stroke call by passing
-// color and line width as arguments
-// default color and width will be restored afterwards
-pen.rect([200, 200], 300, 100)
-   .stroke('#f00', 1);
-
-// draw filled ellipse along with it's outline using 
-// an origin point and a radius vector
-pen.ellipse([200, 200], [100, 50])
-   .stroke()
+pen.circle([250, 250], 100)
    .fill();
